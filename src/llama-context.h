d91ce513c1b7664a567f9c8e06a0f216c5c0c4d0
AlHeloween <alheloween@gmail.com>
2026-01-18 08:29:25 +0800

    Update

--- src/llama-context.h
+++ src/llama-context.h
@@ -370,8 +370,4 @@ private:
     mutable int32_t n_eval   = 0; // number of eval calls
 
     mutable int32_t n_reused = 0; // number of times the previous graph was reused
-
-    // Aurora compute reduction integration
-    struct llama_aurora_context_params* aurora_params = nullptr;
-
-private:
+};
